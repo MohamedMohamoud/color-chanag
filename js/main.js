095744bc68e46101b8b3f37b865b90605149eaa9
mohamedmohamoud <mfrag228@gmail.com>
2022-07-24 08:07:05 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var buttonEl = document.querySelector(".btn");
+var divEl =document.querySelector(".mydiv");
+var colors =["red","green","blue"];
+var i =0
+buttonEl.onclick =function(){
+    divEl.style.backgroundColor = colors[i];
+    i++;
+    if(i == 3){
+        i =0
+    }
+}
